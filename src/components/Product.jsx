7660f6b6451779f1ded4084f09585a04079470de
Tenvy <87992922+Tenvy@users.noreply.github.com>
2023-03-04 22:12:12 +0800

    adding meta img

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,7 +6,6 @@ export const Product = (props) => {
   const [focusedProduct, setFocusedProduct] = useState(null);
   return (
     <div id="product" className="z-[1] flex flex-col h-screen items-center">
-
       <div className="flex flex-col items-center mt-20 py-10">
         <h1 className="font-bold text-3xl md:text-4xl">OUR PRODUCTS</h1>
         <div className="bg-white h-[1px] w-[60%] flex my-3">
